cd48fd68d033eabc0399eb03e5eee13f7555f206
Parth Gupta <parthgupta023@gmail.com>
2022-01-28 20:33:49 -0500

    Added an archive button to archive calls

--- src/components/ActivityFeed.jsx
+++ src/components/ActivityFeed.jsx
@@ -8,17 +8,37 @@ const getCalls = () => fetch("https://aircall-job.herokuapp.com/activities")
                         .then((data) => {return data;});
 
 
+const ArchiveButton = ({call}) => {
+    return (
+    <button onClick={archiveCall(call.id)}>Archive</button>
+    )
+
+}
+
+
+const archiveCall = (callID) => {
+    const handle = () => {
+
+    }
+}
+
 
 const Display = ({calls}) => {
     return (
         <div>
-            {calls.filter(call => !call.is_archived).map(call => 
-                    <ActivityDetail key={call.id} call={call}/>
-                )
-            }
+            {calls.filter(call => !call.is_archived).map(function(call) {
+                    return (
+                        <React.Fragment key={call.id}>
+                        <ActivityDetail call={call}/>
+                        <ArchiveButton call={call} />
+                        </React.Fragment>
+                    )
+                    })
+            } 
         </div>
     )
 }
+
 
 const ActivityFeed = () => {
     const [ callsList, setCallsList ] = useState([])
@@ -26,10 +46,15 @@ const ActivityFeed = () => {
     const handleCalls = () => {
         getCalls().then((data) => setCallsList(data));
     }
+
+    const handleArchives = () => {
+
+    }
     
     return (
         <div className='activity-feed'>
-            <button onClick={handleCalls}>Activity </button>
+            <button onClick={handleCalls}>Show Activity </button>
+            <button onClick={handleArchives}>Show archive</button>
             <ul>
                 <Display calls={callsList}/>
             </ul>
